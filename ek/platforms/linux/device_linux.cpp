f2c3551932b86d5928ccacc57ee729158ca92f59
Elias Ku <deadbabe@gmail.com>
2021-03-20 18:34:38 +0300

    Add main for linux

--- ek/platforms/linux/device_linux.cpp
+++ ek/platforms/linux/device_linux.cpp
@@ -1,4 +1,12 @@
 #include <ek/app/device.hpp>
+#include <ek/app/app.hpp>
+
+// entry point
+int main(int argc, char* argv[]) {
+    ::ek::app::g_app.args = {argc, argv};
+    ::ek::main();
+    return 0;
+}
 
 namespace ek {
 
