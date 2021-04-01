4ba3b05526805b80c52729eff2e3128307b1db62
eliasku <deadbabe@gmail.com>
2021-04-02 00:08:25 +0300

    methods for small array

--- ekx/core/ek/ds/SmallArray.hpp
+++ ekx/core/ek/ds/SmallArray.hpp
@@ -9,20 +9,51 @@ struct SmallArray {
 
     static_assert(MaxCount <= 0x10000);
 
-    unsigned size = 0;
-    T data[MaxCount];
+    unsigned _size = 0;
+    T _data[MaxCount];
 
-    inline void push(T el) {
-        EK_ASSERT_R2(size < MaxCount);
-        data[size++] = el;
+    inline void push_back(T el) {
+        EK_ASSERT(_size < MaxCount);
+        _data[_size++] = el;
     }
 
     inline T* begin() {
-        return data;
+        return _data;
     }
 
     inline T* end() {
-        return data + size;
+        return _data + _size;
+    }
+
+    [[nodiscard]]
+    inline unsigned size() const {
+        return _size;
+    }
+
+    [[nodiscard]]
+    inline const T* data() const {
+        return _data;
+    }
+
+    [[nodiscard]]
+    inline T* data() {
+        return _data;
+    }
+
+    [[nodiscard]]
+    inline T back() const {
+        EK_ASSERT(_size > 0);
+        return *(_data + _size - 1);
+    }
+
+    inline T& operator[](unsigned i) {
+        EK_ASSERT(i < _size);
+        return *(_data + i);
+    }
+
+    inline const T& operator[](unsigned i) const {
+        EK_ASSERT(i < _size);
+        return *(_data + i);
     }
 };
 
