164dba4e921dd01ea25d5774dd6bd8f907dd1282
eliasku <deadbabe@gmail.com>
2021-10-28 21:26:54 +0300

    ios: ATT request

--- plugins/admob/ek.js
+++ plugins/admob/ek.js
@@ -40,8 +40,9 @@ function setup(project) {
                     //GADDelayAppMeasurementInit: true,
                     GADApplicationIdentifier: project.ios.admob_app_id,
                     SKAdNetworkItems: [
-                        {SKAdNetworkIdentifier: "cstr6suwn9.skadnetwork"},
+                        // https://developers.google.com/admob/ios/ios14?hl=en-US#skadnetwork
                         // list of third-party ad buyers: https://developers.google.com/admob/ios/3p-skadnetworks
+                        {SKAdNetworkIdentifier: "cstr6suwn9.skadnetwork"},
                         {SKAdNetworkIdentifier: "4fzdc2evr5.skadnetwork"},
                         {SKAdNetworkIdentifier: "2fnua5tdw4.skadnetwork"},
                         {SKAdNetworkIdentifier: "ydx93a7ass.skadnetwork"},
@@ -55,15 +56,16 @@ function setup(project) {
                         {SKAdNetworkIdentifier: "f38h382jlk.skadnetwork"},
                         {SKAdNetworkIdentifier: "hs6bdukanm.skadnetwork"},
                         {SKAdNetworkIdentifier: "prcb7njmu6.skadnetwork"},
+                        {SKAdNetworkIdentifier: "v4nxqhlyqp.skadnetwork"},
                         {SKAdNetworkIdentifier: "wzmmz9fp6w.skadnetwork"},
                         {SKAdNetworkIdentifier: "yclnxrl5pm.skadnetwork"},
-                        {SKAdNetworkIdentifier: "4468km3ulz.skadnetwork"},
                         {SKAdNetworkIdentifier: "t38b2kh725.skadnetwork"},
                         {SKAdNetworkIdentifier: "7ug5zh24hu.skadnetwork"},
                         {SKAdNetworkIdentifier: "9rd848q2bz.skadnetwork"},
                         {SKAdNetworkIdentifier: "n6fk4nfna4.skadnetwork"},
                         {SKAdNetworkIdentifier: "kbd757ywx3.skadnetwork"},
                         {SKAdNetworkIdentifier: "9t245vhmpl.skadnetwork"},
+                        {SKAdNetworkIdentifier: "4468km3ulz.skadnetwork"},
                         {SKAdNetworkIdentifier: "2u9pt9hc89.skadnetwork"},
                         {SKAdNetworkIdentifier: "8s468mfl3y.skadnetwork"},
                         {SKAdNetworkIdentifier: "av6w8kgt66.skadnetwork"},
@@ -71,9 +73,12 @@ function setup(project) {
                         {SKAdNetworkIdentifier: "ppxm28t8ap.skadnetwork"},
                         {SKAdNetworkIdentifier: "424m5254lk.skadnetwork"},
                         {SKAdNetworkIdentifier: "uw77j35x4d.skadnetwork"},
+                        {SKAdNetworkIdentifier: "578prtvx9j.skadnetwork"},
+                        {SKAdNetworkIdentifier: "4dzt52r2t5.skadnetwork"},
                         {SKAdNetworkIdentifier: "e5fvkxwrpn.skadnetwork"},
+                        {SKAdNetworkIdentifier: "8c4e2ghe7u.skadnetwork"},
                         {SKAdNetworkIdentifier: "zq492l623r.skadnetwork"},
-                        {SKAdNetworkIdentifier: "3qcr597p9d.skadnetwork"},
+                        {SKAdNetworkIdentifier: "3qcr597p9d.skadnetwork"}
                     ],
                 }
             ]
