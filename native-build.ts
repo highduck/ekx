6718dfe87b2157284fec36bab0bcd9a0bf031b88
eliasku <deadbabe@gmail.com>
2023-02-22 18:03:13 +0300

    ci: fix build flags for main native buuld

--- native-build.ts
+++ native-build.ts
@@ -1,7 +1,15 @@
 import {build} from "./modules/cmake/mod.js";
 
-// build tools
+// build main packages, plugins and tools
 await build({
+    definitions: {
+        EKX_BUILD_DEV_TOOLS: "ON",
+        EKX_BUILD_TESTS: "ON",
+        EKX_BUILD_COVERAGE: "OFF",
+        EKX_BUILD_EXTERNAL_TESTS: "OFF",
+        EKX_INCLUDE_EXAMPLES: "ON",
+        EKX_INCLUDE_PLUGINS: "ON",
+    },
     test: true,
     debug: false
 });
